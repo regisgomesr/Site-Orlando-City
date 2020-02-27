659a52277a0e4f0dfb0386142da3bbebc87f6618
Regis Ribeiro <regisgomesr@gmail.com>
2020-02-27 15:45:59 -0300

    Estilizando Menu e input search.

--- js/efeitos.js
+++ js/efeitos.js
@@ -44,4 +44,17 @@ $(document).ready(function(){
 
     });
 
-});
+    $('#btn-bars').on('click', function(){
+       $('header').toggleClass('open-menu')
+    })
+
+    $('#menu-mobile-mask, .btn-close').on('click', function(){
+        $('header').removeClass('open-menu')
+    })
+
+    $('#btn-search').on('click', function(){
+        $('header').toggleClass('open-search')
+        $('#input-search-mobile').focus();
+    })
+
+})
